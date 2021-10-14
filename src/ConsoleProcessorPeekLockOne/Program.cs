692c1c587429bd470af7ed8e987d071f42676b50
Jan de Vries <jan@jan-v.nl>
2021-10-14 21:09:53 +0200

    Added logger to sql

--- src/ConsoleProcessorPeekLockOne/Program.cs
+++ src/ConsoleProcessorPeekLockOne/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProcessLogger;
 
 namespace ConsoleProcessorPeekLockOne
 {
@@ -23,6 +25,7 @@ namespace ConsoleProcessorPeekLockOne
 
             Console.WriteLine("Press any key to end the application");
             Console.ReadKey();
+            await workerInstance.Stop();
         }
 
         static IHostBuilder CreateDefaultBuilder()
@@ -35,24 +38,106 @@ namespace ConsoleProcessorPeekLockOne
                 .ConfigureServices(services =>
                 {
                     services.AddSingleton<Worker>();
+                    services.AddTransient<LogRecord>();
                 });
         }
 
 
         internal class Worker
         {
+            // connection string to your Service Bus namespace
+            static string connectionString;
+
+            // name of your Service Bus queue
+            static string queueName;
+
+
+            // the client that owns the connection and can be used to create senders and receivers
+            static ServiceBusClient client;
+
+            // the processor that reads and processes messages from the queue
+            static ServiceBusProcessor processor;
+
             private readonly ILogger<Worker> logger;
 
             public Worker(
                 IConfiguration configuration,
                 ILogger<Worker> logger)
             {
+                connectionString = configuration["ServiceBus:ConnectionString"];
+                queueName = configuration["ServiceBus:QueueName"];
                 this.logger = logger;
             }
 
             public async Task DoWork()
             {
+                // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+                // of the application, which is best practice when messages are being published or read
+                // regularly.
+                //
 
+                // Create the client object that will be used to create sender and receiver objects
+                client = new ServiceBusClient(connectionString);
+
+                // create a processor that we can use to process the messages
+                processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+                try
+                {
+                    // add handler to process messages
+                    processor.ProcessMessageAsync += MessageHandler;
+
+                    // add handler to process any errors
+                    processor.ProcessErrorAsync += ErrorHandler;
+
+                    // start processing 
+                    await processor.StartProcessingAsync();
+
+                    logger.LogInformation("Wait for a minute and then press any key to end the processing");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, ex.Message);
+                }
+            }
+
+            public async Task Stop()
+            {
+                try
+                {
+                    // stop processing 
+                    logger.LogInformation("Stopping the receiver...");
+                    await processor.StopProcessingAsync();
+                    logger.LogInformation("Stopped receiving messages");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, ex.Message);
+                }
+                finally
+                {
+                    // Calling DisposeAsync on client types is required to ensure that network
+                    // resources and other unmanaged objects are properly cleaned up.
+                    await processor.DisposeAsync();
+                    await client.DisposeAsync();
+                }
+
+            }
+            // handle received messages
+            private async Task MessageHandler(ProcessMessageEventArgs args)
+            {
+                string body = args.Message.Body.ToString();
+                logger.LogInformation($"{body}");
+
+                // complete the message. messages is deleted from the queue. 
+                await args.CompleteMessageAsync(args.Message);
+            }
+
+            // handle any errors when receiving messages
+            private Task ErrorHandler(ProcessErrorEventArgs args)
+            {
+                logger.LogInformation(args.Exception.ToString());
+                return Task.CompletedTask;
             }
         }
     }
---
+++ src/ProcessLogger/LogRecord.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ProcessLogger
+{
+    public class LogRecord
+    {
+        private readonly string connectionString;
+        private const string Insert = @"INSERT INTO [dbo].[Logging]
+           ([Id]
+           ,[Processor]
+           ,[MessageId]
+           ,[Created])
+     VALUES
+           (@Id
+           ,@Processor
+           ,@MessageId
+           ,@Created";
+
+        public LogRecord(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public async Task Store(LogRecord.Entity record)
+        {
+            await using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(Insert, new { Id = record.Id, Processor = record.Processor, MessageId = record.MessageId, Created = record.Created });
+        }
+
+        public class Entity
+        {
+            public Guid Id = Guid.NewGuid();
+            public DateTime Created { get; } = DateTime.UtcNow;
+
+            public int MessageId { get; }
+            public string Processor { get; }
+
+            public Entity(int messageId, string processor)
+            {
+                MessageId = messageId;
+                Processor = processor;
+            }
+        }
+    }
+}
