2f830ec36a98597c8ac0190e57c9c7d7f6681fff
Jan de Vries <jan@jan-v.nl>
2021-10-14 20:10:58 +0200

    Created a producer project

---
+++ src/Producer/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Producer
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var host = CreateDefaultBuilder().Build();
+
+            // Invoke Worker
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var workerInstance = provider.GetRequiredService<Worker>();
+            await workerInstance.DoWork();
+
+            host.Run();
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+
+        static IHostBuilder CreateDefaultBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(app =>
+                {
+                    app.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<Worker>();
+                });
+        }
+    }
+
+    /// <summary>
+    /// Implementation from the Service Bus Messaging documentation site: https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues?WT.mc_id=AZ-MVP-5003246
+    /// </summary>
+    internal class Worker
+    {
+        // connection string to your Service Bus namespace
+        private readonly string connectionString = "<NAMESPACE CONNECTION STRING>";
+
+        // name of your Service Bus queue
+        private readonly string queueName = "<QUEUE NAME>";
+
+        // number of messages to be sent to the queue
+        private readonly int numOfMessages;
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        static ServiceBusSender sender;
+
+        
+
+        public Worker(IConfiguration configuration)
+        {
+            connectionString = configuration["ServiceBus:ConnectionString"];
+            queueName = configuration["ServiceBus:QueueName"];
+            numOfMessages = int.Parse(configuration["ServiceBus:NumberOfMessagesToAdd"]);
+        }
+
+        public async Task DoWork()
+        {
+            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+            // of the application, which is best practice when messages are being published or read
+            // regularly.
+            //
+            // Create the clients that we'll use for sending and processing messages.
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+            // create a batch 
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"{i}")))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
